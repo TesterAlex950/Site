f6c590a92a100177cc38fda79f215357d6f940ba
TesterAlex950 <Vikrotiking@gmail.com>
2025-07-26 18:59:18 +0300

    optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import { Toaster } from '@/components/ui/toaster';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,16 +16,26 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={`${inter.variable}`}>
       <Head>
         <title>PPC & Media Buying Portfolio</title>
-        <meta name="description" content="The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects." />
-        <meta property="og:title" content="PPC & Media Buying Portfolio" />
-        <meta property="og:description" content="The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects." />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="PPC & Media Buying Portfolio" />
-        <meta name="twitter:description" content="The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects." />
+        <meta
+          name='description'
+          content='The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects.'
+        />
+        <meta property='og:title' content='PPC & Media Buying Portfolio' />
+        <meta
+          property='og:description'
+          content='The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects.'
+        />
+        <meta property='og:type' content='website' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='PPC & Media Buying Portfolio' />
+        <meta
+          name='twitter:description'
+          content='The personal portfolio of a beginner-level PPC and media buying intern, showcasing skills and projects.'
+        />
       </Head>
       <Component {...pageProps} />
       <Toaster />
+      <SpeedInsights />
     </div>
   );
 }
